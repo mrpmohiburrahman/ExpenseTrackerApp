43f599c93166000c7521b42fbd9c4cc3466c1e59
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-26 18:45:35 +0600

    simple line chart

--- App/screens/Goals/GoalsScreen.tsx
+++ App/screens/Goals/GoalsScreen.tsx
@@ -1,12 +1,98 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import { View, Dimensions, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import LineChart from 'react-native-simple-line-chart';
 
-const GoalsScreen = () => {
+export default function App() {
+  const data = [
+    {
+      y: 10,
+      x: new Date('2020-01-01').getTime(),
+      extraData: {
+        formattedValue: '0',
+        formattedTime: '2020-01-01',
+      },
+    },
+    {
+      y: 20,
+      x: new Date('2020-01-02').getTime(),
+      extraData: {
+        formattedValue: '20',
+        formattedTime: '2020-01-02',
+      },
+    },
+    {
+      y: 15,
+      x: new Date('2020-01-03').getTime(),
+      extraData: {
+        date: new Date('2020-01-03').getTime(),
+        formattedValue: '15$',
+        formattedTime: '2020-01-03',
+      },
+    },
+    {
+      y: 35,
+      x: new Date('2020-01-04').getTime(),
+      extraData: {
+        formattedValue: '35$',
+        formattedTime: '2020-01-04',
+      },
+    },
+    {
+      y: 5,
+      x: new Date('2020-01-06').getTime(),
+      extraData: {
+        formattedValue: '35$',
+        formattedTime: '2020-01-04',
+      },
+    },
+  ];
   return (
-    <View>
-      <Text>GoalsScreen</Text>
-    </View>
-  );
-};
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <LineChart
+          extraConfig={{
+            alwaysShowActivePoint: false,
+            initialActivePoint: 0,
+          }}
+          lines={[
+            {
+              data,
+              activePointConfig: {
+                color: 'black',
+                showVerticalLine: true,
+                verticalLineWidth: 48,
+              },
 
-export default GoalsScreen;
+              lineColor: 'red',
+              lineWidth: 2,
+              curve: 'monotone',
+
+              endPointConfig: {
+                color: 'red',
+                radius: 10,
+                animated: true,
+              },
+              activePointComponent: point => {
+                return (
+                  <View
+                    style={{
+                      backgroundColor: 'pink',
+                      padding: 10,
+                      borderRadius: 10,
+                    }}>
+                    <Text style={{ color: 'white' }}>{point?.extraData?.formattedValue}</Text>
+                    <Text style={{ color: 'white' }}>{point?.extraData?.formattedTime}</Text>
+                  </View>
+                );
+              },
+            },
+          ]}
+          backgroundColor={undefined}
+          height={200}
+          width={Dimensions.get('screen').width}
+        />
+      </View>
+    </GestureHandlerRootView>
+  );
+}
