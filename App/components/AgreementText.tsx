996aa7a33f42af4282d4a4e0a59889f88a6c9779
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-27 20:58:34 +0600

    🩹 fix register component

--- App/components/AgreementText.tsx
+++ App/components/AgreementText.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
+import Text from './Text';
+import { SCREEN_WIDTH } from 'App/constants/metrics';
 
 const AgreementText = () => {
   return (
-    <View style={{ flexDirection: 'row', gap: 2 }}>
-      <Text style={{ fontSize: 13, fontWeight: '500' }}>I agree with the</Text>
-      <Text style={{ fontSize: 13, fontWeight: '500', color: '#3570FF' }}>Terms & Conditions</Text>
-      <Text style={{ fontSize: 13, fontWeight: '500' }}>and</Text>
+    <View>
+      <View style={{ flexDirection: 'row', gap: 2 }}>
+        <Text style={{ fontSize: 13, fontWeight: '500' }}>I agree with the</Text>
+        <Text style={{ fontSize: 13, fontWeight: '500', color: '#3570FF' }}>Terms & Conditions</Text>
+        <Text style={{ fontSize: 13, fontWeight: '500' }}>and</Text>
+      </View>
+
       <Text style={{ fontSize: 13, fontWeight: '500', color: '#3570FF' }}>Privacy Policy</Text>
     </View>
   );
