c5c54805c6e301354354494f6ce4ce74d3d2052a
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-27 07:21:49 +0600

    ✨ make bar same height

--- App/screens/Goals/GoalsScreen.tsx
+++ App/screens/Goals/GoalsScreen.tsx
@@ -1,35 +1,70 @@
-import { Circle, LinearGradient, useFont, vec } from '@shopify/react-native-skia';
+import { BlurMask, Circle, Group, LinearGradient, RoundedRect, Shadow, useFont, vec } from '@shopify/react-native-skia';
 import * as React from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import { useDerivedValue, type SharedValue } from 'react-native-reanimated';
 import { Bar, CartesianChart, Line, useChartPressState } from 'victory-native';
 
 import { Text as SKText } from '@shopify/react-native-skia';
-export default function GettingStartedScreen(props: { segment: string }) {
-  const { state, isActive } = useChartPressState({ x: 0, y: { highTmp: 0 } });
 
-  const toolTipFont = useFont(require('@assets/fonts/Inter_18pt-Black.ttf'), 24);
+// const DATA = Array.from({ length: 5 }, (_, i) => ({
+//   day: i,
+//   highTmp: 5 + 30 * Math.random(),
+// }));
+const DATA = (length: number = 10) =>
+  Array.from({ length }, (_, index) => ({
+    month: index + 1,
+    listenCount: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
+    specialCount: 100,
+  }));
+export default function GettingStartedScreen(props: { segment: string }) {
+  const [data, setData] = React.useState(DATA(5));
+  console.log('🚀 ~ GettingStartedScreen ~ data:', data);
+
+  const { state, isActive } = useChartPressState({
+    x: 0,
+    y: { listenCount: 0, specialCount: 100 },
+  });
+
+  const toolTipFont = useFont(require('@assets/fonts/Inter_18pt-Black.ttf'), 14);
+
+  const width = 100;
+  const height = 40;
+  const borderRadius = 30;
 
   const value = useDerivedValue(() => {
-    return '$' + state.y.highTmp.value.value;
+    return '$ ' + state.y.listenCount.value.value;
   }, [state]);
+
   const textXPosition = useDerivedValue(() => {
     if (!toolTipFont) {
       return 0;
     }
     return state.x.position.value - toolTipFont.measureText(value.value).width / 2;
   }, [value, toolTipFont]);
+
   const textYPosition = useDerivedValue(() => {
-    return state.y.highTmp.position.value - 15;
+    return state.y.listenCount.position.value - height * 0.8;
   }, [value]);
+
+  const bgXPosition = useDerivedValue(() => {
+    if (!toolTipFont) {
+      return 0;
+    }
+    return state.x.position.value - width / 2;
+  }, [value, toolTipFont]);
+
+  const bgYPosition = useDerivedValue(() => {
+    return state.y.listenCount.position.value - height * 1.5;
+  }, [value]);
+
   return (
     <SafeAreaView style={styles.safeView}>
       <View style={{ flex: 1, maxHeight: 400, padding: 32 }}>
         <CartesianChart
-          data={DATA}
-          xKey="day"
-          yKeys={['highTmp']}
-          domainPadding={{ left: 30, right: 30 }}
+          data={data}
+          xKey="month"
+          yKeys={['listenCount', 'specialCount']}
+          domainPadding={{ left: 30, right: 30, top: 60 }}
           xAxis={{
             formatXLabel: value => {
               return `${value}`;
@@ -41,23 +76,39 @@ export default function GettingStartedScreen(props: { segment: string }) {
           {({ points, chartBounds }) => (
             <>
               <Bar
-                points={points.highTmp}
+                points={points.specialCount}
                 chartBounds={chartBounds}
                 animate={{ type: 'spring' }}
-                innerPadding={0.33}
+                innerPadding={0.2}
                 roundedCorners={{
                   topLeft: 5,
                   topRight: 5,
                 }}>
-                <LinearGradient start={vec(0, 0)} end={vec(0, 400)} colors={['#efe9de', '#efe9de']} />
+                <LinearGradient start={vec(0, 0)} end={vec(0, 400)} colors={['#f3f2ec', '#f3f2ec']} />
               </Bar>
-              <Line curveType="cardinal" points={points.highTmp} color="#cea868" strokeWidth={3} connectMissingData />
+              <Line
+                curveType="cardinal"
+                points={points.listenCount}
+                color="#cea868"
+                strokeWidth={3}
+                connectMissingData
+              />
               {/* {isActive && <ToolTip x={state.x.position} y={state.y.highTmp.position} />} */}
-              {isActive ? (
-                <>
-                  <SKText font={toolTipFont} color={'black'} x={textXPosition} y={textYPosition} text={value} />
-                  <Circle cx={state.x.position} cy={state.y.highTmp.position} r={8} color={'grey'} opacity={0.8} />
-                </>
+              {true ? (
+                <Group>
+                  <Circle cx={state.x.position} cy={state.y.listenCount.position} r={8} color={'grey'} opacity={0.8} />
+                  <RoundedRect
+                    x={bgXPosition}
+                    y={bgYPosition}
+                    width={width}
+                    height={height}
+                    r={borderRadius}
+                    color="white">
+                    <Shadow dx={12} dy={12} blur={25} color="#363B6414" />
+                    {/* <Shadow dx={-12} dy={-12} blur={25} color="#c7f8ff" /> */}
+                  </RoundedRect>
+                  <SKText x={textXPosition} y={textYPosition} text={value} font={toolTipFont} color={'#ADAFBD'} />
+                </Group>
               ) : null}
             </>
           )}
@@ -70,11 +121,6 @@ export default function GettingStartedScreen(props: { segment: string }) {
 function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
   return <Circle cx={x} cy={y} r={8} color="black" />;
 }
-
-const DATA = Array.from({ length: 5 }, (_, i) => ({
-  day: i,
-  highTmp: 5 + 30 * Math.random(),
-}));
 
 const styles = StyleSheet.create({
   safeView: {
